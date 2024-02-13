145b918737e15f3c7c32933ec8ade8d2a4481fcf
Ahmadxon <ahmadxonahmedov64@gmail.com>
2024-02-13 22:24:04 +0500

    Upload project

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("IELTS score average FOR academic");
+
+Console.Write("Listening score: ");
+string listeningScoreInput = Console.ReadLine()!;
+
+Console.Write("Reading score: ");
+string readingScoreInput = Console.ReadLine()!;
+
+System.Console.Write("Speaking score: ");
+string speakingScoreInput = Console.ReadLine()!;
+
+Console.Write("Writing score: ");
+
+string writingScoreInput = Console.ReadLine()!;
+decimal[] scores={
+    Convert.ToDecimal(readingScoreInput),
+    Convert.ToDecimal(listeningScoreInput),
+    Convert.ToDecimal(writingScoreInput),
+    Convert.ToDecimal(speakingScoreInput)
+    };
+decimal averageScore = scores.Average();
+int scoreInteger = (int)averageScore;
+decimal remainder = averageScore-scoreInteger;
+
+decimal scoreRemainder = remainder switch 
+{
+   _ when remainder < 0.25m => 0,
+   _ when remainder < 0.75m && remainder >= 0.25m => 0.5m,
+   _ => 1, 
+};
+averageScore=scoreInteger+scoreRemainder;
+Console.WriteLine($"Avreage score: {averageScore}");
+
